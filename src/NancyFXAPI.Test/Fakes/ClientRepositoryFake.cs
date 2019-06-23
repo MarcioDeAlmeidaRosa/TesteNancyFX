bb8e53629740c26e92f4065530875a986d875755
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-06-22 23:00:02 -0300

    Implementando mock para gerar valores aleatórios para teste

--- src/NancyFXAPI.Test/Fakes/ClientRepositoryFake.cs
+++ src/NancyFXAPI.Test/Fakes/ClientRepositoryFake.cs
@@ -1,4 +1,5 @@
-﻿using NancyFXAPI.Domain;
+﻿using FizzWare.NBuilder;
+using NancyFXAPI.Domain;
 using NancyFXAPI.Repository.Contracts;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,21 @@ namespace NancyFXAPI.Test.Fakes
 {
     public class ClientRepositoryFake : IRepository<Client>
     {
-        private readonly IList<Client> clients = new List<Client>()
+        private readonly IList<Client> clients;
+
+        public ClientRepositoryFake()
         {
-            new Client{ ID = 1, Name="Marcio de Almeida Rosa" , Age = 35  },
-            new Client{ ID = 2, Name="Xica da Silva a Negra" , Age = 64  },
-        };
+            clients = Builder<Client>
+                .CreateListOfSize(100)
+                .All()
+                .WithConstructor(() => new Client()
+                {
+                    ID = Faker.Number.Even(1, 100),
+                    Name = Faker.Name.FullName(),
+                    Age = Faker.Number.Even(1, 70)
+                })
+           .Build();
+        }
 
         public void Delete(long id)
         {
